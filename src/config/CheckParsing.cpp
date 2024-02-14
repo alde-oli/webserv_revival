67cd4a7fb74acc35ac0f5509b972e4577fa74823
Cécile Duffaut <cecileduffaut21@gmail.com>
2024-02-15 00:22:07 +0100

    Ip adress check

--- src/config/CheckParsing.cpp
+++ src/config/CheckParsing.cpp
@@ -1,1 +1,11 @@
 #include "../../include/config/CheckParsing.hpp"
+
+//Check IP server
+
+bool checkIPServer(ServConfig server, std::string ip)
+{
+	std::string ipAddress = inet_ntoa(server.getAddr().sin_addr);
+	if (LOCALHOST == ipAddress || ipAddress == EXTERNAL_IP)
+		return true;
+	return false;
+}
